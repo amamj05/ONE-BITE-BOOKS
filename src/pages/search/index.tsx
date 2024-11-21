681e94f90026296db5fc0af39a85ddb8f1296557
mj <ui2939@gmail.com>
2024-11-22 00:51:27 +0900

    feat: Book Page

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,13 +1,15 @@
 import SearchableLayout from "@/components/searchable-layout"
 import { ReactNode } from "react"
 import { useRouter } from "next/router";
+import books from '@/mock/books.json'
+import BookItem from '@/components/book-item';
 
 export default function Page() {
     const router = useRouter();
     const { q } = router.query;
     return (
         <div>
-
+            {books.map((book)=><BookItem key={book.id}{...book}/>)}
         </div>
     )
 }
